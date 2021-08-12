1e907733e29985370c96768e28ef03a44f48eb80
Ruchita Kalantri <rkalantri.kalantri@yahoo.com>
2021-08-12 11:25:18 +0530

    initalize project , import init , express , nodemon and body-parser , updated package.json file, created app.js and routes and controller

---
+++ app.js
@@ -0,0 +1,10 @@
+const e = require('express');
+const express = require('express');
+
+const feedRoutes = require('./routes/feed');
+
+const app = express();
+
+app.use('/feed' , feedRoutes);
+
+app.listen(8080);
