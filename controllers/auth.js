ce2a966987885c0124750990f53b7f85edda4a1b
Ruchita Kalantri <rkalantri.kalantri@yahoo.com>
2021-08-15 13:58:43 +0530

    update status code into database

--- controllers/auth.js
+++ controllers/auth.js
@@ -88,3 +88,47 @@ exports.login = (req, res, next) => {
       next(err);
     });
 };
+
+exports.getUserStatus = (req, res, next) => { 
+    User.findById(req.userId)
+        .then(user => {
+            if(!user) {
+                const error = new Error("User Not Found!");
+                error.statusCode = 404;
+                throw error;  
+            }
+            //have user
+            res.status(200).json({status : user.status})
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+              err.statusCode = 500;
+            }
+            next(err);
+          });
+}
+
+exports.updateUserStatus  = (req, res, next) => { 
+    const newStatus = req.body.status;
+
+    User.findById(req.userId)
+    .then(user => {
+        if(!user) {
+            const error = new Error("User Not Found!");
+            error.statusCode = 404;
+            throw error;  
+        }
+        //have user
+        user.status = newStatus;
+        return user.save();
+    })
+    .then( result => {
+        res.status(200).json({message : 'User Updated'})
+    })
+    .catch((err) => {
+        if (!err.statusCode) {
+          err.statusCode = 500;
+        }
+        next(err);
+      });
+}
--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,8 @@ const User = require("../models/user");
 
 const authController = require("../controllers/auth");
 
+const isAuth = require("../middleware/is-auth");
+
 const router = express.Router();
 
 router.put(
@@ -28,6 +30,20 @@ router.put(
   authController.signup
 );
 
-router.post("/login" , authController.login);
+router.post("/login", authController.login);
+
+router.get("/status", isAuth, authController.getUserStatus);
+
+router.patch(
+  "/status",
+  isAuth,
+  [
+      body("status")
+        .trim()
+        .not()
+        .isEmpty()
+   ],
+  authController.updateUserStatus
+);
 
 module.exports = router;
